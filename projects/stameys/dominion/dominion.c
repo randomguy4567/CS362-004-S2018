1a19e3e9f33a404f60bcc5f2445f2c3b402159c0
randomguy4567 <stameys@oregonstate.edu>
2018-04-29 14:32:27 -0700

    Assignment 3 forgetten files!

--- projects/stameys/dominion/dominion.c
+++ projects/stameys/dominion/dominion.c
@@ -1273,7 +1273,7 @@ int updateCoins(int player, struct gameState *state, int bonus)
 
 int cardSmithy( int currentPlayer, int handPos, struct gameState *state){
 	int i;
-     //+3 Cards
+     //+2 Cards
     for (i = 0; i < 2; i++)
 	{
 	  drawCard(currentPlayer, state);
@@ -1287,25 +1287,25 @@ int cardSmithy( int currentPlayer, int handPos, struct gameState *state){
 
 int cardAdventurer(int currentPlayer, int drawntreasure, int *cardDrawn, int *temphand, struct gameState *state){
   int z = 0;
-  while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	*cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (*cardDrawn == copper || *cardDrawn == silver || *cardDrawn == gold)
-	  drawntreasure--;
-	else{
-	  temphand[z]=*cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;	
+  while(drawntreasure<1){
+  	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+  	  shuffle(currentPlayer, state);
+  	}
+  	drawCard(currentPlayer, state);
+  	*cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+  	if (*cardDrawn == copper || *cardDrawn == silver || *cardDrawn == gold)
+     drawntreasure++;
+  	else{
+  	  temphand[z]=*cardDrawn;
+  	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+  	  z++;
+  	}
+  }
+  while(z-1>=0){
+	 state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	 z=z-1;
+  }
+  return 0;	
 	
 }
 
@@ -1347,12 +1347,13 @@ int cardRemodel(int currentPlayer, int choice1, int choice2, int handPos, struct
 	
     j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
-	{
-	  return -1;
-	}
+    if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
+    //if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(state->hand[currentPlayer][choice2]))
+  	{
+  	  return -1;
+  	}
 
-      gainCard(choice1, state, 0, currentPlayer);
+      gainCard(choice2, state, 0, currentPlayer);
 
       //discard card from hand
       discardCard(handPos, currentPlayer, state, 0);
